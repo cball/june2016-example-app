4869e69b85909107557313c919feac1c311ad9df
Chris Ball <chris@echobind.com>
2016-06-20 16:37:09 -0400

    Add image and RVSP

--- app/pods/organizations/route.js
+++ app/pods/organizations/route.js
@@ -1,21 +1,32 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
+  ajax: Ember.inject.service(),
+
   model() {
-    return [
-      Ember.Object.create({ id: 1, name: 'Org 1' }),
-      Ember.Object.create({ id: 2, name: 'Org 2' })
+    const requests = [
+      this.get('ajax').request('https://api.github.com/orgs/emberjs?access_token=9a9cee03c07a59b1387b5480b3b8c946a47d1eec'),
+      this.get('ajax').request('https://api.github.com/orgs/ciena-blueplanet?access_token=9a9cee03c07a59b1387b5480b3b8c946a47d1eec')
     ];
+
+    return RSVP.all(requests);
+
+    // return [
+    //   Ember.Object.create({ id: 1, name: 'Org 1' }),
+    //   Ember.Object.create({ id: 2, name: 'Org 2' })
+    // ];
   },
 
   actions: {
     star(organization) {
       // if we aren't sure its an ember object
-      // Ember.set(organization, 'isStarred' true);
+      const isStarred = Ember.get(organization, 'isStarred');
+      Ember.set(organization, 'isStarred', !isStarred);
 
       // if we have ember object
       // organization.set('isStarred', true);
-      organization.toggleProperty('isStarred');
+      // organization.toggleProperty('isStarred');
     }
   }
 });
