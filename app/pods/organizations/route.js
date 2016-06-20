13bde0ac9057f81ac559d22fb891ab0311f3a868
Chris Ball <chris@echobind.com>
2016-06-20 17:08:40 -0400

    add afterModel example and catch

--- app/pods/organizations/route.js
+++ app/pods/organizations/route.js
@@ -10,13 +10,22 @@ export default Ember.Route.extend({
       this.get('ajax').request('https://api.github.com/orgs/ciena-blueplanet?access_token=9a9cee03c07a59b1387b5480b3b8c946a47d1eec')
     ];
 
-    return RSVP.all(requests);
+    return RSVP.all(requests)
+      .catch((e) => {
+        console.log(e);
+      });
 
     // return [
     //   Ember.Object.create({ id: 1, name: 'Org 1' }),
     //   Ember.Object.create({ id: 2, name: 'Org 2' })
     // ];
   },
+
+  // if you need something after model has resolve
+  // afterModel(model) {
+    // get a property from model and make another request
+    // return this.get('ajax').request(somethingElse)
+  // }
 
   actions: {
     star(organization) {
