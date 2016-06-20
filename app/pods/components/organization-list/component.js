09184cd21cdb274ca9cf1b18a60ed8f889460898
Chris Ball <chris@echobind.com>
2016-06-20 14:44:57 -0400

    computed properties

--- app/pods/components/organization-list/component.js
+++ app/pods/components/organization-list/component.js
@@ -1,5 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'ul'
+  tagName: 'ul',
+
+  organizations: null,
+  starredOrganizations: Ember.computed.filterBy('organizations', 'isStarred', true),
+  hasStars: Ember.computed.gt('starredOrganizations.length', 0),
+
+  init() {
+    this._super(...arguments);
+
+    // Dont need this part if using ember-prop-types
+    // this.organizations = this.organizations || [];
+    // this.set('organizations', this.get('organizations') || []);
+  }
 });
