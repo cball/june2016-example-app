14b5681f3fc05754976e01cdb80ac3c92686aa4d
Chris Ball <chris@echobind.com>
2016-06-21 15:20:57 -0400

    Add some more integration tests

--- tests/integration/pods/organization-list/component-test.js
+++ tests/integration/pods/organization-list/component-test.js
@@ -5,6 +5,7 @@ import {
   it
 } from 'ember-mocha';
 import {
+  beforeEach,
   describe
 } from 'mocha';
 import hbs from 'htmlbars-inline-precompile';
@@ -17,11 +18,42 @@ describeComponent(
   },
   function() {
     describe('no organizations', function() {
-      it('shows "No items."', function() {
+      beforeEach(function() {
         this.render(hbs`{{organization-list}}`);
-        const listText = this.$('li').text().trim();
+      });
 
+      it('shows "No items."', function() {
+        const listText = this.$('li').text().trim();
         expect(listText).to.equal('No items.');
+      });
+    });
+
+    describe('with organizations', function() {
+      const organizations = [
+        { id: 1, name: 'Org 1' },
+        { id: 2, name: 'Org 2', isStarred: true }
+      ];
+
+      beforeEach(function() {
+        this.set('organizations', organizations);
+        this.render(hbs`{{organization-list organizations=organizations}}`);
+      });
+
+      it('renders the organizations as list items', function() {
+        expect(this.$('li.organization')).to.have.length(organizations.length);
+      });
+
+      it('shows the organization name', function() {
+        // you can also use string interpolation here and do:
+        // const [firstOrganizationName] = organizations;
+        // `li:contains(${firstOrganizationName.name})`
+        const firstOrganization = this.$('li:contains(Org 1)');
+        expect(firstOrganization).to.have.length(1);
+      });
+
+      it('adds a class of "starred" if isStarred is true', function() {
+        const org2 = this.$('li:contains(Org 2)');
+        expect(org2.hasClass('starred')).to.be.ok;
       });
     });
 
