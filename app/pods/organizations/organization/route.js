01aab90363dfec53541b12d3d9a68ae9db7bd946
Chris Ball <chris@echobind.com>
2016-06-22 11:35:24 -0400

    show loading and error states

--- app/pods/organizations/organization/route.js
+++ app/pods/organizations/organization/route.js
@@ -1,9 +1,19 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
-  github: Ember.inject.service(),
+// shortening chaining via destructuring
+// const { Route } = Ember;
+// const {
+//   inject: { service }
+// } = Ember;
+
+// shortening chaning via ember-cli-shims
+import service from 'ember-service/inject';
+import Route from 'ember-route';
+
+export default Route.extend({
+  github: service(),
 
   model({ login }) {
-    return this.get('github').request(`orgs/${login}`);
+    return this.get('github').request(`org s/${login}`);
   }
 });
